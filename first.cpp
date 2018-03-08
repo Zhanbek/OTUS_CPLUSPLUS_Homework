375e2dfe6014119bc51f3b203ef03d1d621306c9
zhanbek.sarsenov <zhanbek.sarsenov@gmail.com>
2018-03-09 02:46:53 +0600

    Hello, World message file has been added to file

--- first.cpp
+++ first.cpp
@@ -3,6 +3,7 @@
 #include <spdlog/spdlog.h>
 
 int main(int argc, char *argv[]) {
+   std::cout<< "Hello, World!" << std::endl;
    auto logger = spdlog::stdout_logger_mt("console");
    logger->info("Current patch version is {}", getPatchVersion());
    return 0;
