0a2016aaf322f7b8b748a0c6b6d67739f1331141
Zhanbek <zhanbek.sarsenov@gmail.com>
2018-03-09 21:04:05 +0600

    error finding 3

--- first.cpp
+++ first.cpp
@@ -6,10 +6,8 @@ int main(int argc, char *argv[]) {
    std::clog<< "Hello, World!" << std::endl;    
    
    auto logger = spdlog::stdout_logger_mt("console");
-   logger->info("Current patch version is {}.{}.{}", 
-                getVersionMajor(),
-                getVersionMinor(), 
-                geVersionPatch()
+   logger->info("Current patch version is {}", 
+                getVersionPatch()
                );
    return 0;
 }
